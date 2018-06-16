fcd534f14a68652cac4dea83baca16799e8b8eeb
tnovak123 <timothy.m.novak@gmail.com>
2018-06-16 16:29:52 -0500

    Todos 1 & 2 Complete

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -24,6 +24,7 @@ public class JobController {
     public String index(Model model, int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
+        model.addAttribute("job", jobData.findById(id));
 
         return "job-detail";
     }
