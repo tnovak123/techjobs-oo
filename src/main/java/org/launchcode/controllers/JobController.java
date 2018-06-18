e14d6cf652e5a3eac54d86ea3a18f949760d4f62
tnovak123 <timothy.m.novak@gmail.com>
2018-06-17 21:00:36 -0500

    Todo 6 completed, however, there is in consistent data when populating the new page.

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -6,6 +6,7 @@ import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -37,35 +38,30 @@ public class JobController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @Valid JobForm jobForm, Errors errors) {
+    public String add(Model model, @ModelAttribute Job newJob, @Valid JobForm jobForm, Errors errors) {
 
         // TODO #6 - Validate the JobForm model, and if valid, create a
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
-        String aName = new String();
-        String aEmployer = new String();
-        String aLocation = new String();
-        String aSkill = new String();
-        String aPosition = new String();
+        if (errors.hasErrors()){
+            model.addAttribute(new JobForm());
+            return "new-job";
+        }
 
-        model.addAttribute("name", aName);
-        model.addAttribute("employer.value", aEmployer);
-        model.addAttribute("location.value", aLocation);
-        model.addAttribute("coreCompetency.value", aSkill);
-        model.addAttribute("positionType.value", aPosition);
+        model.addAttribute("jobForm", jobForm);
 
-        Employer bEmployer = new Employer(aEmployer);
-        Location bLocation = new Location(aLocation);
-        PositionType bPosition = new PositionType(aPosition);
-        CoreCompetency bSkill = new CoreCompetency(aSkill);
+        newJob.setName(jobForm.getName());
+        newJob.setEmployer(jobForm.getEmployers().get(jobForm.getEmployerId()));
+        newJob.setLocation(jobForm.getLocations().get(jobForm.getLocationId()));
+        newJob.setCoreCompetency(jobForm.getCoreCompetencies().get(jobForm.getCoreCompetencyId()));
+        newJob.setPositionType(jobForm.getPositionTypes().get(jobForm.getPositionTypeId()));
 
-        Job newJob = new Job(aName, bEmployer, bLocation, bPosition, bSkill);
 
         jobData.add(newJob);
 
-        model.addAttribute("job", newJob);
+//        model.addAttribute("job", newJob);
 
-        return "redirect:";
+        return "redirect:?id=" + newJob.getId();
 
     }
 }
